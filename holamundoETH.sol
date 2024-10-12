38bb0822fe81018125527bc7f2625d8f41282a9c
NicolasFrancoMendoza <brayamfrme@unisabana.edu.co>
2024-10-11 22:31:57 -0500

    primer contrato Remix

---
+++ holamundoETH.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// 0x11De77A7e6F317f54B1eD9445e10aBc29372378a
+
+pragma solidity 0.8.24; //Defino la versión del compilador
+
+//a partir de acá coloco la lógica de mi contrato
+contract holamundoETH {
+    string public saludo = "hola desde Nueva Mapachelandia"; //declarando el contenido de mi variable "saludo"
+}
+
